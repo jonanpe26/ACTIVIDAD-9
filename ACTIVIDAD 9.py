5a36501048c8ba4c9e9cc59f344ef1fd8c996cd9
jonan <heber2025sandoval@gmail.com>
2025-07-23 18:49:31 -0600

    1RO

--- ACTIVIDAD 9.py
+++ ACTIVIDAD 9.py
@@ -1,1 +1,11 @@
 #ACTIVIDA 9
+cliente={}
+def lugares(clientes):
+    if len(clientes)==0:
+        return 0
+    cliente=clientes[0]
+    return len(cliente[cliente]["destinos"])+lugares(clientes[1:])
+
+def maximo(clientes):
+    max
+print("Ingrese numeero de clientes")
